9883d511286c70b687d6f418b818b1cd0e2f8b58
Phillipi Unger Lino <phillipi.lino@gmail.com>
2022-07-29 10:01:51 -0300

    Add logger and firebase

---
+++ packages/solfacil_tools_sdk/lib/log/log_manager.dart
@@ -0,0 +1,33 @@
+import 'package:solfacil_tools_sdk/log/request_log_event.dart';
+import 'package:solfacil_tools_sdk/log/sol_logger.dart';
+
+class LogManager {
+  SolLogger? logger;
+  bool debugMode = false;
+
+  static final LogManager shared = LogManager.internal();
+
+  factory LogManager() => shared;
+
+  LogManager.internal() : logger = SolLogger();
+
+  logRequestEvent(RequestLogEvent event) {
+    if (!debugMode) return;
+    logger?.logRequestEvent(event);
+  }
+
+  logWarning(dynamic message) {
+    if (!debugMode) return;
+    logger?.logWarning(message);
+  }
+
+  logInfo(dynamic message) {
+    if (!debugMode) return;
+    logger?.logInfo(message);
+  }
+
+  logError(dynamic message) {
+    if (!debugMode) return;
+    logger?.logError(message);
+  }
+}
---
+++ packages/solfacil_tools_sdk/test/coverage_test.dart
@@ -0,0 +1,16 @@
+/// *** GENERATED FILE - ANY CHANGES WOULD BE OBSOLETE ON NEXT GENERATION *** ///
+
+/// Helper to test coverage for all project files
+// ignore_for_file: unused_import
+import 'package:solfacil_tools_sdk/trackers/external_trackers_interface.dart';
+import 'package:solfacil_tools_sdk/trackers/solfacil_trackers_manager.dart';
+import 'package:solfacil_tools_sdk/trackers/solfacil_firebase_sdk.dart';
+import 'package:solfacil_tools_sdk/firebase/firebase_analytics_adapter.dart';
+import 'package:solfacil_tools_sdk/firebase/firebase_database_adapter.dart';
+import 'package:solfacil_tools_sdk/firebase/firebase_crashlytics_adapter.dart';
+import 'package:solfacil_tools_sdk/firebase/firebase_performance_adapter.dart';
+import 'package:solfacil_tools_sdk/log/log_manager.dart';
+import 'package:solfacil_tools_sdk/log/sol_logger.dart';
+import 'package:solfacil_tools_sdk/log/request_log_event.dart';
+
+void main() {}
