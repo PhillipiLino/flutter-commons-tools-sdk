de273ab0bac1a52346ce507bd0db23528173f9f2
Phillipi Unger Lino <phillipi.lino@gmail.com>
2022-08-23 10:27:45 -0300

    Add launch darkly to sdk

---
+++ packages/solfacil_tools_sdk/lib/remote_config/external_remote_config_interface.dart
@@ -0,0 +1,12 @@
+abstract class IExternalRemoteConfigs {
+  Future<void> setUser(
+    String userId,
+    String userEmail, {
+    Map<String, dynamic>? aditionalInfos,
+  });
+  Future<void> changeEnvironment(String environmentMobileKey);
+  Future<bool> getBool(String key, bool defaultValue);
+  Future<int> getInt(String key, int defaultValue);
+  Future<double> getDouble(String key, double defaultValue);
+  Future<String> getString(String key, String defaultValue);
+}
---
+++ packages/solfacil_tools_sdk/lib/remote_config/solfacil_launch_darkly_sdk.dart
@@ -0,0 +1,63 @@
+library solfacil_firebase_sdk;
+
+import 'package:solfacil_tools_sdk/launch_darkly/launch_darkly_adapter.dart';
+import 'package:solfacil_tools_sdk/log/log_manager.dart';
+
+import 'external_remote_config_interface.dart';
+
+class SolfacilLaunchDarklySDK extends IExternalRemoteConfigs {
+  late final LaunchDarklyAdapter launchDarklyAdapter;
+
+  SolfacilLaunchDarklySDK(String mobileKey) {
+    _initializeApp(mobileKey);
+  }
+
+  Future _initializeApp(String mobileKey) async {
+    try {
+      launchDarklyAdapter = LaunchDarklyAdapter()..configure(mobileKey);
+      return true;
+    } catch (e) {
+      LogManager.shared.logError('LAUNCH_DARKLY_SDK: $e');
+      return null;
+    }
+  }
+
+  @override
+  Future<bool> getBool(String key, bool defaultValue) async {
+    return await launchDarklyAdapter.getBool(key, defaultValue);
+  }
+
+  @override
+  Future<double> getDouble(String key, double defaultValue) async {
+    return await launchDarklyAdapter.getDouble(key, defaultValue);
+  }
+
+  @override
+  Future<int> getInt(String key, int defaultValue) async {
+    return await launchDarklyAdapter.getInt(key, defaultValue);
+  }
+
+  @override
+  Future<String> getString(String key, String defaultValue) async {
+    return await launchDarklyAdapter.getString(key, defaultValue);
+  }
+
+  @override
+  Future<void> changeEnvironment(String environmentMobileKey) async {
+    await launchDarklyAdapter.stopClient();
+    return await launchDarklyAdapter.configure(environmentMobileKey);
+  }
+
+  @override
+  Future<void> setUser(
+    String userId,
+    String userEmail, {
+    Map<String, dynamic>? aditionalInfos,
+  }) async {
+    return await launchDarklyAdapter.setUser(
+      userId,
+      userEmail,
+      aditionalInfos: aditionalInfos,
+    );
+  }
+}
