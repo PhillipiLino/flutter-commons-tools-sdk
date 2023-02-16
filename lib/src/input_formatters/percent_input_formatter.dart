c80fffce5e02d2335aa4d90d7b7178a23d79ee98
Matheus Bereta <matheus_bereta@hotmail.com>
2023-02-16 16:28:22 -0300

    :bug: Fixing PercentInputFormatter

--- lib/src/input_formatters/percent_input_formatter.dart
+++ lib/src/input_formatters/percent_input_formatter.dart
@@ -4,8 +4,9 @@ class PercentInputFormatter extends TextInputFormatter {
   @override
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
-    int value =
-        newValue.selection.baseOffset == 0 ? 0 : int.parse(newValue.text);
+    int value = newValue.selection.baseOffset == 0
+        ? 0
+        : int.parse(newValue.text.onlyDigits());
     final formatter = NumberFormat(null, 'pt_Br');
     String newText = formatter.format(value);
 
